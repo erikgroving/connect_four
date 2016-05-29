b153afe2cf0b7a1b7e24497cced8cda76fe919bc
Erik <egmeade@live.com>
2016-05-28 22:41:43 -0400

    Moved main to main.cpp, added few more functions

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "connectfour.h"
+
+int main() {
+    string setting, var_val;
+    string  p1, p2;
+    string  my_bot;
+    int     timebank;
+    int     time_per_move;
+    int     my_bot_id;
+    int     cols;
+    int     rows = -1;
+
+
+    getSettings (p1, p2, my_bot, timebank, time_per_move,
+            my_bot_id, cols, rows);
+
+    gameOn(rows, cols);
+
+    return 0;
+}
